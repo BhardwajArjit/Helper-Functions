b2ad54417b5fde2da6ed2b5358a9ec500d887215
BhardwajArjit <iamarjitbhardwaj@gmail.com>
2023-10-24 16:19:36 +0530

    add translate

--- computer_vision.py
+++ computer_vision.py
@@ -11,3 +11,11 @@ def rescaleFrame(frame, scale):
     dimensions = (width, height)
 
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+def translate(img, x, y):
+    """Translate the image by x and y"""
+
+    translation = np.float32([[1, 0, x], [0, 1, y]])
+    dimensions = (img.shape[1], img.shape[0])
+
+    return cv.warpAffine(img, translation, dimensions)
