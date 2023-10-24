e9ee81222ca7fa43f2c9bd3e21c2cc74ff7f2d6f
BhardwajArjit <iamarjitbhardwaj@gmail.com>
2023-10-24 16:12:24 +0530

    add cv functions

--- computer_vision.py
+++ computer_vision.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 import numpy as np
 
-
-def rescale(frame, scale):
-    """Rescale the frame by x and y"""
+# OpenCV functions
+def rescaleFrame(frame, scale):
+    """Rescale the frame of an image or video"""
 
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
